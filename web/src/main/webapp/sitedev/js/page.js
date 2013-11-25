e2cc090e3276da3f6562fab491b0207337e0133c
nathan <nathan@6c957121-43e2-4d78-bcb6-0dd8c699bb15>
2013-11-25 18:17:23 +0000

    git-svn-id: svn://code.careeropts.com/rurse/trunk@73 6c957121-43e2-4d78-bcb6-0dd8c699bb15

---
+++ web/src/main/webapp/sitedev/js/page.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+function writeHeader() {
+
+	$('#header').html('<img src="img/logo.png" width="197" height="89" alt=""/><p class="rurse">RURSE </p>');
+	$('#header').append('<p>  <a href="resume.html">resume</a> <a href="jobs.html">jobs</a> courses books</p>');
+}
